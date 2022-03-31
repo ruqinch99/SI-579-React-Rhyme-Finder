cf096d39ac1754593e5044143ded1a1c0e592d27
ruqinch99 <91104721+ruqinch99@users.noreply.github.com>
2022-03-31 06:02:03 -0400

    Delete Components directory

--- Components/SavedWords.js
+++
@@ -1,9 +0,0 @@
-const SavedWords = (props) => {
-    if (props.savedWords.length) {
-        return ( < div className = "col" > Saved words: < span id = "saved_words" > {props.savedWords.join(', ')} < /span></div > );
-    } else {
-        return ( < div className = "col" > Saved words: < span id = "saved_words" > (none) < /span></div > );
-    }
-}
-
-export default SavedWords;
